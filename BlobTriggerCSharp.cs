76c4a097865d4eddc4499b8ff9383de1f7f37f8d
Cloudshikshak <41804489+Cloudshikshak@users.noreply.github.com>
2020-02-14 23:14:52 -0600

    Image thumbnail
    ImageSharp nuget package with blob trigger and blob out (C#)

---
+++ BlobTriggerCSharp.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+
+namespace Cloudshikshak.Function
+{
+    public static class BlobTriggerCSharp
+    {
+        [FunctionName("BlobTriggerCSharp")]
+        public static void Run([BlobTrigger("image-input/{name}", Connection = "AzureWebJobsStorage")]Stream inBlob, string name,
+        [Blob("image-output/thumbnail-{name}", FileAccess.Write, Connection = "AzureWebJobsStorage")] Stream outBlob,
+        ILogger log)
+        {
+            log.LogInformation($"New image uploaded in image-input container: {name}");
+            try
+            {
+                using (var image = Image.Load(inBlob))
+                {
+                    image.Mutate(x => x.Resize(new ResizeOptions { 
+                        Size = new Size { 
+                            Height = 100, 
+                            Width = 100 }, 
+                        Mode = ResizeMode.Crop 
+                    }));
+        
+                    using (var ms = new MemoryStream())
+                    {
+                        image.SaveAsPng(outBlob);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message, null);
+            }
+        }
+    }
+}
