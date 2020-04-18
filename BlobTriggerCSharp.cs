37b9b3608365339f206e398156de15e259fa7a74
Cloudshikshak <41804489+Cloudshikshak@users.noreply.github.com>
2020-04-17 20:10:28 -0500

    Update BlobTriggerCSharp.cs

--- BlobTriggerCSharp.cs
+++ BlobTriggerCSharp.cs
@@ -12,9 +12,9 @@ namespace Cloudshikshak.Function
     public static class BlobTriggerCSharp
     {
         [FunctionName("BlobTriggerCSharp")]
-        public static void Run([BlobTrigger("image-input/{name}", Connection = "afcmasterstorageaccount_STORAGE")]Stream inBlob, 
+        public static void Run([BlobTrigger("image-input/{name}", Connection = "<<STORAGE_ACCOUNT_CONNECTION_STRING>>")]Stream inBlob, 
         string name,
-        [Blob("image-output/thumbnail-{name}", FileAccess.Write, Connection = "afcmasterstorageaccount_STORAGE")] Stream outBlob,
+        [Blob("image-output/thumbnail-{name}", FileAccess.Write, Connection = "<<STORAGE_ACCOUNT_CONNECTION_STRING>>")] Stream outBlob,
         ILogger log)
         {
             log.LogInformation($"New image uploaded in image-input container: {name}");
